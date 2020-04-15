ffc510bb9b660cfde1eddaec37c36288224a6b37
James Leyden <jamesedward1024@gmail.com>
2020-04-14 21:04:44 -0500

    mvp

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,18 +1,17 @@
 //Complete the necessary code in this file
-// import useState
 import React from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
 
-const PostsPage = () => {
-  // set up state for your data
+// pass the data from App.js down as props then map through the data
+const PostsPage = props => {
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {props.postData.map(p => (
+        <Post key={p.imageUrl} post={p} />
+      ))}
     </div>
   );
 };
 
 export default PostsPage;
-
